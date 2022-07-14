fed786fe530787f45e635a28c2d9305425ce76e4
Friedrich G. Froebel <froebel.business@gmail.com>
2022-07-14 23:22:42 +0200

    Added logger to pycarrot.__init__

--- pycarrot/__init__.py
+++ pycarrot/__init__.py
@@ -1,2 +1,23 @@
+import logging
+import datetime
+
 from . import feat_analysis
 from . import modelling
+
+
+# Set up root logger, and add a file handler to root logger
+logging.basicConfig(
+    filename=f"./logs/log_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M')}.log",
+    level=logging.DEBUG,
+    format="[%(levelname)1.1s %(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# Create logger for handling stream
+stream_logger = logging.getLogger("stream")
+stream_logger.setLevel(logging.INFO)
+shandler = logging.StreamHandler()
+shandler.setFormatter(
+    logging.Formatter("[%(levelname)1.1s %(asctime)s] %(message)s", "%Y-%m-%d %H:%M:%S")
+)
+stream_logger.addHandler(shandler)
